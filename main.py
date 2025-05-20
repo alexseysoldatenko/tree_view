6beccda6f18602583d4d0b668d721598bea58e74
alexseysoldatenko <126325350+alexseysoldatenko@users.noreply.github.com>
2025-05-20 14:30:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import main_window
 from PyQt5.QtWidgets import QApplication
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = main_window.Window()
     window.show()
-    sys.exit(app.exec_ ())
+    sys.exit(app.exec_())
